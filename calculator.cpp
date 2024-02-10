f08b00c90515c1d371c0a2c3085a11de8bd399ff
memesofting <bibitayoadekunle00@gmail.com>
2024-02-10 10:26:42 +0100

    switching to phone

--- calculator.cpp
+++ calculator.cpp
@@ -4,9 +4,35 @@
  * main: entry point for simple calculator
  * Return: 0 on success
  */
+using namespace std;
 
 int main()
 {
+	char operation;
+	double a;
+	double b;
+
+	cout << "What arithmethic operation would you like to perform" << endl;
+	cout << "Enter 'a' for addition\n";
+	cout << "'s' for subtraction\n";
+	cout << "'d' for division\n";
+	cout << "'m' for multiplication\n";
+	cin >> operation;
+	if (operation = 'd')
+	{
+		cout << "Enter numerator\n";
+		cin >> a;
+		cout << "Enter denominator\n";
+		cin >> b;
+		cout << a + "divided by" + b + " = "<< divide(a, b) << endl;
+	}
+	else
+	{
+		cout << "Enter first number\n";
+		cin >> a;
+		cout << "Enter second number\n";
+		cin >> b;
+	}
 	return 0;
 }
 
@@ -25,7 +51,7 @@ int mul(int x, int y)
 	return (x * y);
 }
 
-int div(int x, int y)
+double divide(double x, double y)
 {
 	return (x / y);
 }
